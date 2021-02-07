0d0584a6fc36a826c2f65ffa64d4e03e3c60a5b0
kalemena <kalemena@free.fr>
2021-02-07 21:49:01 +0100

    ESP32 - add websocket

--- sketches/esp32-09-web-relay-th/esp32-09-web-relay-th.ino
+++ sketches/esp32-09-web-relay-th/esp32-09-web-relay-th.ino
@@ -11,6 +11,10 @@
 #include "SPIFFS.h"
 
 #include <WiFi.h>
+#include <WebSocketsServer.h>
+
+WiFiServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
 
 // HTU21
 #include <Wire.h>
@@ -22,8 +26,6 @@ float lastHumidity = 0.0;
 // Relays
 // holds 16 bits
 unsigned int relayState = 0;
-
-WiFiServer server(80);
 
 #define DEBUG true
 #define Serial if(DEBUG)Serial
@@ -42,11 +44,22 @@ void setup() {
   Serial.println("Initializing ...");
   Serial.println();
 
+  // ===== File System
+  delay(10);
+  if(!SPIFFS.begin(false)){
+    Serial.println("SPIFFS Mount Failed");
+    return;
+  } else {
+    Serial.println("SPIFFS Mount OK");
+  }
+  listDir(SPIFFS, "/", 3);
+
   // ===== HTU21D
   while(!htu.begin()) {
     Serial.println("Couldn't find sensor ...");
     delay(100);
   }
+  Serial.println("HTU21D Sensor found !");
 
   // ===== Relays on 74HC595
   // Set pins to output so you can control the shift register
@@ -72,17 +85,9 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  // ===== File System
-  delay(10);
-  if(!SPIFFS.begin(false)){
-    Serial.println("SPIFFS Mount Failed");
-    return;
-  } else {
-    Serial.println("SPIFFS Mount OK");
-  }
-  listDir(SPIFFS, "/", 3);
-
-  // HTTP start
+  webSocket.begin();
+  webSocket.onEvent(websocket_event);
+  
   server.begin();
 }
 
@@ -90,61 +95,115 @@ int value = 0;
 
 void loop(){
 
-  delay(5000);
-  operation_read_TH();
-  
- WiFiClient client = server.available();   // listen for incoming clients
-
-  if (client) {                             // if you get a client,
-    Serial.println("New Client.");           // print a message out the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
-
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 5 on.<br>");
-            client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 5 off.<br>");
-
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {    // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          digitalWrite(5, HIGH);               // GET /H turns the LED on
-        }
-        if (currentLine.endsWith("GET /L")) {
-          digitalWrite(5, LOW);                // GET /L turns the LED off
-        }
-      }
-    }
-    // close the connection:
-    client.stop();
-    Serial.println("Client Disconnected.");
-  }
+  webSocket.loop();
+//  
+//  delay(5000);
+//  operation_read_TH();
+//  
+// WiFiClient client = server.available();   // listen for incoming clients
+//
+//  if (client) {                             // if you get a client,
+//    Serial.println("New Client.");           // print a message out the serial port
+//    String currentLine = "";                // make a String to hold incoming data from the client
+//    while (client.connected()) {            // loop while the client's connected
+//      if (client.available()) {             // if there's bytes to read from the client,
+//        char c = client.read();             // read a byte, then
+//        Serial.write(c);                    // print it out the serial monitor
+//        if (c == '\n') {                    // if the byte is a newline character
+//
+//          // if the current line is blank, you got two newline characters in a row.
+//          // that's the end of the client HTTP request, so send a response:
+//          if (currentLine.length() == 0) {
+//            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+//            // and a content-type so the client knows what's coming, then a blank line:
+//            client.println("HTTP/1.1 200 OK");
+//            client.println("Content-type:text/html");
+//            client.println();
+//
+//            // the content of the HTTP response follows the header:
+//            client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 5 on.<br>");
+//            client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 5 off.<br>");
+//
+//            // The HTTP response ends with another blank line:
+//            client.println();
+//            // break out of the while loop:
+//            break;
+//          } else {    // if you got a newline, then clear currentLine:
+//            currentLine = "";
+//          }
+//        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+//          currentLine += c;      // add it to the end of the currentLine
+//        }
+//
+//        // Check to see if the client request was "GET /H" or "GET /L":
+//        if (currentLine.endsWith("GET /H")) {
+//          digitalWrite(5, HIGH);               // GET /H turns the LED on
+//        }
+//        if (currentLine.endsWith("GET /L")) {
+//          digitalWrite(5, LOW);                // GET /L turns the LED off
+//        }
+//      }
+//    }
+//    // close the connection:
+//    client.stop();
+//    Serial.println("Client Disconnected.");
+//  }
 }
 
+void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
+  const uint8_t* src = (const uint8_t*) mem;
+  Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+  for(uint32_t i = 0; i < len; i++) {
+    if(i % cols == 0) {
+      Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+    }
+    Serial.printf("%02X ", *src);
+    src++;
+  }
+  Serial.printf("\n");
+}
 
-// TOOLS
+void websocket_event(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+
+    switch(type) {
+        case WStype_DISCONNECTED:
+            Serial.printf("[%u] Disconnected!\n", num);
+            break;
+        case WStype_CONNECTED:
+            {
+                IPAddress ip = webSocket.remoteIP(num);
+                Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+
+        // send message to client
+        webSocket.sendTXT(num, "Connected");
+            }
+            break;
+        case WStype_TEXT:
+            Serial.printf("[%u] get Text: %s\n", num, payload);
+
+            // send message to client
+            // webSocket.sendTXT(num, "message here");
+
+            // send data to all connected clients
+            // webSocket.broadcastTXT("message here");
+            break;
+        case WStype_BIN:
+            Serial.printf("[%u] get binary length: %u\n", num, length);
+            hexdump(payload, length);
+
+            // send message to client
+            // webSocket.sendBIN(num, payload, length);
+            break;
+    case WStype_ERROR:      
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+      break;
+    }
+}
+
+// ===== TOOLS
 
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
     Serial.printf("Listing directory: %s\r\n", dirname);
